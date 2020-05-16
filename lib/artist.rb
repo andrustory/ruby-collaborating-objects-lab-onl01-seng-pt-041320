07bd571bfcf9c25911fa7c26a02eac05c8a15326
Andres Carvajal <jc1993box@hotmail.com>
2020-05-16 08:18:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+   self.songs << song
+  end
+  
+  def self.create(name)
+    artist = self.new(name)
+    artist
+  end
+
+   def self.find(name)
+    self.all.detect {|artist| artist.name == name}
+  end
+
+   def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
+
+   def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+
+end
+
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+def initialize(name)
+  @name=name
+  @@all << self
+end 
+
+def self.all
+  @@all
+end 
+
+ def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+end
+
+end
+
+
